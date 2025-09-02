10ba4b14f662cde28bd0b8773c255281bca92dd0
Андрей Кузнецов <andreismith2014@yandex.ru>
2025-09-02 22:05:19 +0500

    ver_02.09.2025

---
+++ blogicum/blog/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+from django.utils import timezone
+
+
+def basic_set():
+    """Базовый набор выбранных постов"""
+    return (
+        Post.objects.select_related('author', 'category', 'location')
+        .filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True,
+        ).order_by('-pub_date')
+    )
+
+
+def index(request):
+    """Главная страница"""
+    # Получаем последние 5 опубликованных постов по условиям
+    post_list = basic_set()[:5]
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def post_detail(request, post_id):
+    """Отображение описания выбранной записи"""
+    post = get_object_or_404(
+        Post.objects.select_related('category')
+        .filter(id=post_id, is_published=True,
+                pub_date__lte=timezone.now(), category__is_published=True)
+    )
+
+    return render(request, 'blog/detail.html', {'post': post})
+
+
+def category_posts(request, category_slug):
+    """Отображение постов категории"""
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+
+    # Получаем опубликованные посты данной категории
+    post_list = basic_set().filter(category=category)
+
+    context = {
+        'category': category,
+        'post_list': post_list,
+    }
+    return render(request, 'blog/category.html', context)
---
+++ blogicum/pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import AboutView, RulesView
+
+
+app_name = 'pages'
+
+
+urlpatterns = [
+    path('about/', AboutView.as_view(template_name='pages/about.html'),
+         name='about'),
+    path('rules/', RulesView.as_view(template_name='pages/rules.html'),
+         name='rules'),
+]
